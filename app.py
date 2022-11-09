284787957bdc2925809fb9b400ddf4af585ddcfc
YVD7 <yashdawande7@gmail.com>
2022-11-09 18:26:08 +0530

    update

--- app.py
+++ app.py
@@ -14,4 +14,24 @@ def home():
     except Exception as e:
         raise e
 
-@app.route("/report",)
+@app.route("/predict",methods=["post"])
+@cross_origin()
+def predict():
+    try:
+        if request.method == "POST":
+            try:
+            #                 ['Cement (component 1)(kg in a m^3 mixture)',
+            #  'Blast Furnace Slag (component 2)(kg in a m^3 mixture)',
+            #  'Fly Ash (component 3)(kg in a m^3 mixture)',
+            #  'Water  (component 4)(kg in a m^3 mixture)',
+            #  'Superplasticizer (component 5)(kg in a m^3 mixture)',
+            #  'Coarse Aggregate  (component 6)(kg in a m^3 mixture)',
+            #  'Fine Aggregate (component 7)(kg in a m^3 mixture)',
+            #  'Age (day)',
+            #  'Concrete compressive strength(MPa, megapascals) ']
+                  Cement (component 1)(kg in a m^3 mixture)
+
+            except Exception as e:
+                pass
+    except Exception as e:
+        pass
